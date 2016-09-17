b3defcb0e55c73f3e9419e243b72c14e14e5c206
Rene Baumgartner <rene@redox-labs.com>
2016-09-17 12:45:13 +0200

    Time Manager Test

---
+++ Assets/Scripts/Editor/TimeManagerInspector.cs
@@ -0,0 +1,32 @@
+﻿// REDOX Game Labs 2016
+
+#region INCLUDES
+using UnityEngine;
+#endregion
+
+public class TimeManagerInspector : MonoBehaviour 
+{
+	/// <summary>
+	/// Used for initialisation and creating references
+	/// </summary>
+	void Awake () 
+	{
+	
+	}
+	
+	/// <summary>
+	/// Used for setting up MonoBehaviour
+	/// </summary>
+	void Start () 
+	{
+	
+	}
+	
+	/// <summary>
+	/// Update Loop, called once per frame
+	/// </summary>
+	void Update () 
+	{
+	
+	}
+}
