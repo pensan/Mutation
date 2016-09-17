314d3363a68113079bccefb18fad613e58478605
Rene Baumgartner <rene@redox-labs.com>
2016-09-17 12:50:57 +0200

    Time Manager

---
+++ Assets/Scripts/Simulation/TimeManager.cs
@@ -0,0 +1,49 @@
+﻿// REDOX Game Labs 2016
+
+#region INCLUDES
+using UnityEngine;
+#endregion
+
+public class TimeManager : MonoBehaviour 
+{
+    private float originalFixedPhysicsTime;
+    private float originalTimeScale;
+
+    private void Awake()
+    {
+        originalFixedPhysicsTime = Time.fixedDeltaTime;
+        originalTimeScale = Time.timeScale;
+
+        CustomTime = 1.0f;
+    }
+
+    private float _customTime;
+    public float CustomTime
+    {
+        get
+        {
+            return _customTime;
+        }
+
+        set
+        {
+            _customTime = value;
+            Time.timeScale = _customTime;
+
+            if (_customTime > 0.0f)
+            {
+                Time.fixedDeltaTime = originalFixedPhysicsTime / _customTime;
+            }
+            else
+            {
+                Time.fixedDeltaTime = 0.0f;
+            }
+        }
+    }
+
+    void OnDestroy()
+    {
+        Time.fixedDeltaTime = originalFixedPhysicsTime;
+        Time.timeScale = originalTimeScale;
+    }
+}
---
+++ Assets/Scripts/Simulation/TimeManagerInspector.cs
@@ -0,0 +1,23 @@
+﻿// REDOX Game Labs 2016
+
+#region INCLUDES
+using UnityEngine;
+using UnityEditor;
+#endregion
+
+[CustomEditor(typeof(TimeManager))]
+public class TimeManagerInspector : Editor 
+{
+    private float customTime = 1.0f;
+    public override void OnInspectorGUI()
+    {
+        TimeManager timeManger = (TimeManager)target;
+
+        customTime = EditorGUILayout.Slider(timeManger.CustomTime, 0.0f, 10.0f);
+
+        if (customTime != timeManger.CustomTime)
+        {
+            timeManger.CustomTime = customTime;
+        }
+    }     
+}
