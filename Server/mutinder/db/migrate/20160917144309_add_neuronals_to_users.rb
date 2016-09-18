a0ce54e870fc340faf46584282a5c2bbb00a8921
Terry <theresa@redox-labs.com>
2016-09-18 15:28:06 +0200

    Query update

--- Server/mutinder/db/migrate/20160917144309_add_neuronals_to_users.rb
+++ Server/mutinder/db/migrate/20160917144309_add_neuronals_to_users.rb
@@ -1,7 +1,7 @@
 class AddNeuronalsToUsers < ActiveRecord::Migration[5.0]
 
   def change
-    add_column :users, :neuronal_network, :json,  null: false, default: '{}'
+    add_column :users, :neuronal_network, :json, null: true
   end
 
 end
