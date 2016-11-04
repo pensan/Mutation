ff4115d87d42fe0d3d166dffaadf85f86872da31
BenWahl <hello@skodone.at>
2016-11-04 16:42:51 +0100

    More GUI, Bacteria Portraits

--- Assets/Scripts/GUI/Level/AgentPanel.cs
+++ Assets/Scripts/GUI/Level/AgentPanel.cs
@@ -1,12 +1,22 @@
-﻿using UnityEngine;
+﻿
+#region INCLUDES
+using UnityEngine;
 using UnityEngine.UI;
+using System;
 using System.Collections;
+using System.Collections.Generic;
+#endregion
 
+[System.Serializable]
 public class AgentPanel : MonoBehaviour
 {
+	public List<Sprite> portraitsBody1;
+	public List<Sprite> portraitsBody2;
+	public Image testImage;
     public InputField NameText;
     public Text GenerationText;
     public Image AgentImage;
+
 
     void Start()
     {
@@ -22,6 +32,8 @@ public class AgentPanel : MonoBehaviour
             agent = value;
             NameText.text = agent.FirstName;
             GenerationText.text = agent.GenerationName;
+			AgentImage.sprite = Instantiate(portraitsBody1[getRandomImage(agent.Appearance.body.name)]);
+			AgentImage.color = agent.Appearance.body.color;
         }
     }
 
@@ -30,4 +42,15 @@ public class AgentPanel : MonoBehaviour
         this.Agent.FirstName = name;
     }
 
+	public int getRandomImage(string bodyType) {
+		if (bodyType == "Body") {
+			System.Random r = new System.Random ();
+			return r.Next (0, portraitsBody1.Count);
+		} else if (bodyType == "Body2") {
+			System.Random r = new System.Random ();
+			return r.Next (0, portraitsBody2.Count);
+		} else {
+			return 0;
+		}
+	}	
 }
